b3f68ff4223ab1c9a01d7d14aa170a3b0f878a2d
bib4maru <kirillosanov@yandex.ru>
2023-10-14 23:20:47 +0300

    Practice 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,4 +6,8 @@ fun main(args: Array<String>) {
     //Practice 2
 //    val program = MovieTicketPrice()
 //    program.Start()
+
+    //Practice 3
+    val program = TemperatureConverter()
+    program.Start()
 }
